c8c61718827bdd664adb8d62286e4bc7e0aa5ae1
Julius Vincent Babies <julius@familie-babies.de>
2025-10-17 12:09:55 +0200

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<a href="/resize-test">resize test</a>
---
+++ src/routes/resize-test/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let scale = $state(1);
+</script>
+
+<div class="flex flex-col w-full h-full">
+    <div class="flex flex-row">
+        <input
+                class="flex-1"
+                bind:value={scale}
+                type="range"
+                min="0.1"
+                max="2"
+                step="0.01"
+        >
+        <span>{scale}</span>
+    </div>
+
+    <div class="mt-4">
+        Hallo
+    </div>
+</div>
